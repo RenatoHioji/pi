76ac030cb767e4c366943f43fd05b320a1411e65
extremehi <okamoto.hioji@gmail.com>
2024-04-16 21:57:05 -0300

    backend: adding cors for react port 3001

--- src/node/backend/index.js
+++ src/node/backend/index.js
@@ -2,11 +2,14 @@ import express from "express"
 import moongoose from "mongoose"
 import LoginController from "./pais/controller/PessoaController.js"
 import AlarmeController from "./pais/controller/AlarmeController.js"
-import Notification from "./notification/notification.js"
+import cors from "cors"
 const app = express()
 moongoose.connect("mongodb://localhost:27017/pi-pais")
 app.use(express.json());
 
+app.use(cors({
+    origin: "http://localhost:3001"
+}))
 app.use("/", LoginController)
 app.use("/", AlarmeController)
 
