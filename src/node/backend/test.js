65e194781755da37b794ffff9d8af0c2b0ed40dd
saulo <78312710+Saulo217@users.noreply.github.com>
2024-05-24 21:42:55 -0300

    update: remove unecessary files and create .env.example

--- src/node/backend/test.js
+++
@@ -1,4 +0,0 @@
-fetch('http://localhost:3000/api/alarme/661da1313c7f42f828fd9888')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error('Error fetching data:', error))
