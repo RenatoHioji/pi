105a4089359ee2955b4cfb4a8400bac50051594b
renato <okamoto.hioji@gmail.com>
2024-05-30 10:58:41 -0300

    teste

--- index.js
+++ index.js
@@ -2,14 +2,36 @@ import 'dotenv/config'
 import express from "express"
 import moongoose from "mongoose"
 import cors from "cors"
+import session from 'express-session'
+import AlarmeController from "./src/controllers/AlarmeController.js"
+import PessoaController from './src/controllers/PessoaController.js'
+import ListaController from "./src/controllers/ListaController.js"
+import JogoController from "./src/controllers/JogoController.js"
+import CategoriaController from "./src/controllers/CategoriaController.js"
+import SubcategoriaController from "./src/controllers/SubcategoriaController.js"
+import ItemController from "./src/controllers/ItemController.js"
 
 const PORT = process.env.PORT || 3001
 const MONGO_DB_URL = process.env.LOCAL_MONGO_DB_URL || process.env.DOCKER_MONGO_DB_URL
 
 const app = express()
-app.use(express.json());
+
+app.use(express.json())
 app.use(cors({ origin: `http://localhost:${PORT}` }))
 app.use(express.static('src/public'))
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true 
+}));
+app.use("/", PessoaController)
+app.use("/", AlarmeController)
+app.use("/", ListaController)
+app.use("/", JogoController)
+app.use("/", CategoriaController)
+app.use("/", SubcategoriaController)
+app.use("/", ItemController)
 
 app.set("views", "./src/views/")
 app.set("view engine", "ejs")
@@ -46,68 +68,6 @@ app.get("/addCards", (req, res) => {
 
 app.get("/listas", (req, res) => {
     res.render("listas")
-})
-
-app.listen(PORT, err => {
-    try {
-        console.log(`Server litening on http://localhost:${PORT}`)
-    } catch(err){
-        console.log(err)
-    }
-})
-import 'dotenv/config'
-import express from "express"
-import moongoose from "mongoose"
-import cors from "cors"
-import multer from 'multer'
-import session from 'express-session'
-import AlarmeController from "./src/controllers/AlarmeController.js"
-import PessoaController from './src/controllers/PessoaController.js'
-import ListaController from "./src/controllers/ListaController.js"
-import JogoController from "./src/controllers/JogoController.js"
-import CategoriaController from "./src/controllers/CategoriaController.js"
-import SubcategoriaController from "./src/controllers/SubcategoriaController.js"
-import ItemController from "./src/controllers/ItemController.js"
-const PORT = process.env.PORT || 3001
-
-const app = express()
-app.use(express.json());
-app.use(cors({ origin: `http://localhost:${PORT}` }))
-app.use(express.static('src/public'))
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true 
-}));
-
-
-
-
-app.set("views", "./src/views/")
-app.set("view engine", "ejs")
-
-moongoose.connect(process.env.MONGODB_URI + process.env.DB)
-
-//Rotas
-app.use("/", PessoaController)
-app.use("/", AlarmeController)
-app.use("/", ListaController)
-app.use("/", JogoController)
-app.use("/", CategoriaController)
-app.use("/", SubcategoriaController)
-app.use("/", ItemController)
-
-
-app.get("/", (req, res) => {
-    res.render("splash")
-})
-
-app.get("/main", (req, res) => {
-    res.render("main")
-})
-
-app.get("/rotina", (req, res) => {
-    res.render("rotina")
 })
 
 app.listen(PORT, err => {
