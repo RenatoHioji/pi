924a54209f6a103d4ab6e64e480973921fc3b902
Saulo de freitas <78312710+Saulo217@users.noreply.github.com>
2024-06-03 18:54:23 -0300

    Frontend (#1)
    * CRUD: CRUD teste da aplicação
    
    * Fix: Moving docker yml
    
    * frontend: Criação do App React
    
    * frontend: base frontend
    
    * Development: reestruturação das pastas
    
    * backend: configuring db
    
    * Backend: Login Model
    
    * backend: Adding Alarme/foreign keying com login
    
    * backend: mudando de SQL para nosql
    
    * backend: register funcionando, mas sem retornar o corpo ao usuário
    
    * backend: arrumando o post para retornar o body
    
    * backend: pegar o perfil do usuário pelo id
    
    * backend: login de forma simples
    
    * backend: Criação do alarme dentro da conta dos pais
    
    * backend: desenvolvimento do delete de alarmes
    
    * backend: adding get all by person id + test.js for fetching
    
    * backend: adding update method
    
    * backend: bug fix
    
    * backend: notification endpoint
    
    * backend: adding cors for react port 3001
    
    * frontend: cadastro pessoa
    
    * backend: adding sessions
    
    * frontend: loginform
    
    * Adding jwt for backend
    
    * backend fixing jwt
    
    * frontend: adding perfil and login
    
    * feat: setup project dirs
    
    * update: remove react and react-dom libs and install ejs lib
    
    * feat: added descrption and install intructions to README.md
    
    * feat: added endpoints to endpoints.md
    
    * feat: fix problems showed by markdownlint vscode extension
    
    * feat: added svg icons
    
    * feat: added css
    
    * create splash screen
    
    * desenvolvendo o note em partials
    
    * trabalhando na main page
    
    * feat: create splash screen
    
    * updating main
    
    * feat: remove wrong file
    
    * feat: added stop instructions for docker
    
    * fix: a typo showed by markdownlint vscode extension
    
    * feat: added sample animation to splash screen sun
    
    * trabalhando na main page
    
    * fix: change splash animation
    
    * trabalhando na pagina de rotina
    
    * trabalhando no css da rotina
    
    * Página de rotina pronta
    
    * criando a pagina de emocoes
    
    * nova versão de design
    
    * criando a função de mostrar videos
    
    * modal finalizado e criação da página de criar card
    
    * Desenvolvendo adicionar cards e listas
    
    * fix: refactoring screens
    
    * fix: refactoring mainPage layout
    
    * update: separate css
    
    * finalizando as paginas
    
    * ajustando erros de css
    
    * ajustando as fontes
    
    * fix: css problems
    
    ---------
    
    Co-authored-by: Renato Odake <89861693+RenatoHioji@users.noreply.github.com>
    Co-authored-by: renato <okamoto.hioji@gmail.com>
    Co-authored-by: Murilo <127051508+murilo587@users.noreply.github.com>
    Co-authored-by: unknown <analarapupo@gmail.com>

---
+++ index.js
@@ -0,0 +1,81 @@
+import 'dotenv/config'
+import express from "express"
+import moongoose from "mongoose"
+import cors from "cors"
+
+const PORT = process.env.PORT || 3001
+const MONGO_DB_URL = process.env.LOCAL_MONGO_DB_URL || process.env.DOCKER_MONGO_DB_URL
+
+const app = express()
+app.use(express.json());
+app.use(cors({ origin: `http://localhost:${PORT}` }))
+app.use(express.static('src/public'))
+
+app.set("views", "./src/views/")
+app.set("view engine", "ejs")
+
+moongoose.connect(MONGO_DB_URL + process.env.DB)
+
+app.get("/", (req, res) => {
+    res.render("splash")
+})
+
+app.get("/main", (req, res) => {
+    res.render("main")
+})
+
+app.get("/rotina", (req, res) => {
+    res.render("rotina")
+})
+
+app.get("/necessidades", (req, res) => {
+    res.render("necessidades")
+})
+
+app.get("/alimentos", (req, res) => {
+    res.render("alimentos")
+})
+
+app.get("/meusCards", (req, res) => {
+    res.render("meusCards")
+})
+
+app.get("/addCards", (req, res) => {
+    res.render("addCards")
+})
+
+app.get("/listas", (req, res) => {
+    res.render("listas")
+})
+
+app.get("/listas_fimDeSemana", (req, res) => {
+    res.render("listas_fimDeSemana")
+})
+
+app.get("/addList", (req, res) => {
+    res.render("addList")
+})
+
+app.get("/dias_da_semana", (req, res) => {
+    res.render("dias_da_semana")
+})
+
+app.get("/praticar", (req, res) => {
+    res.render("praticar")
+})
+
+app.get("/quiz", (req, res) => {
+    res.render("quiz")
+})
+
+app.get("/parabens", (req, res) => {
+    res.render("parabens")
+})
+
+app.listen(PORT, err => {
+    try {
+        console.log(`Server litening on http://localhost:${PORT}`)
+    } catch(err){
+        console.log(err)
+    }
+})
---
+++ src/public/js/splash.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', () => {
+    setTimeout(() => {
+        window.location.href = "/main"
+    }, 1000 * 2)
+})
