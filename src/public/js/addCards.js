a31536b07ef4de9ae2db602573ce9c1baaaf2ac4
Murilo <127051508+murilo587@users.noreply.github.com>
2024-06-03 22:20:13 -0300

    ajustando os inputs

---
+++ src/public/js/addCards.js
@@ -0,0 +1,23 @@
+
+let div_newCard = document.querySelector('.newCard')
+let div_newCard2 = document.querySelector('.newCard2')
+let input_imagem = document.querySelector('#img_card')
+let input_video = document.querySelector('#video_card')
+let btn_enviar = document.querySelector('.addCard')
+let enviar = document.querySelector('#enviar')
+
+
+div_newCard.addEventListener('click', () => {
+    input_imagem.click()
+})
+
+div_newCard2.addEventListener('click', () => {
+    input_video.click()
+})
+
+btn_enviar.addEventListener('click', () => {
+    enviar.click()
+})
+
+
+let btn_addCard = document.querySelector('.addCard')
