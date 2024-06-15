74a5a0f98a8aba74a6ce154fa331f2b8b8ed0740
Murilo <127051508+murilo587@users.noreply.github.com>
2024-06-14 21:52:53 -0300

    game pronto

--- src/public/js/quiz3.js
+++ src/public/js/quiz3.js
@@ -1,8 +1,11 @@
 //lógica do game 1
 
 let correto = document.querySelector('#correto')
+let errado = document.querySelectorAll('#err')
 let check = document.querySelector('.levels .nivel:nth-child(4)')
 let img_check = document.createElement('img')
+let audio_correto = document.querySelector('#audio-correto')
+let audio_erro = document.querySelector('#audio-erro')
 img_check.src = '/svg/png/img-check.png'
 
 correto.addEventListener('click', () => {
@@ -21,4 +24,14 @@ let div_audio = document.querySelector('.audio')
    
 div_audio.addEventListener('click', () => {
      audio.play()
+})
+
+correto.addEventListener('click', () => {
+    audio_correto.play()
+})
+
+errado.forEach(err => {
+    err.addEventListener('click', () => {
+        audio_erro.play()
+})
 })
