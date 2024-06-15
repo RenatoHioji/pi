78bd903b7b171aa5cb9ea0383ae8967a33c37b09
renato <okamoto.hioji@gmail.com>
2024-06-15 13:19:04 -0300

    testeMerge: bd

--- src/controllers/ItemController.js
+++ src/controllers/ItemController.js
@@ -4,6 +4,9 @@ import express from "express"
 import {Item} from "./../models/Item.js"
 import multer from 'multer'
 import path from "path"
+import axios from "axios"
+import FormData from "form-data"
+import { imagem } from "../models/Imagem.js"
 const router = express.Router()
 
 
@@ -26,10 +29,10 @@ const storage = multer.diskStorage({
 })
 const upload = multer({ storage: storage })
 router.post("/categoria/:idCategoria/subcategoria/:idSubcategoria/item", upload.fields([{name: "imagem"}, {name: "audio"}, {name:"video"}]), async (req, res) => {
-
     const imagens = req.files.imagem ? req.files.imagem.map(file => path.join('imagens', file.originalname)) : []
     const audio = req.files.audio ? path.join('audios', req.files.audio[0].originalname) : null
     const video = req.files.video ? path.join('videos', req.files.video[0].originalname) : null
+
     const item = new Item({
         nome,
         classificacao,
@@ -38,7 +41,8 @@ router.post("/categoria/:idCategoria/subcategoria/:idSubcategoria/item", upload.
         audio,
         video
       })
-  
+
+
     const response = await ItemService.Create(
         req.params.idCategoria,
         req.params.idSubcategoria,
@@ -82,20 +86,35 @@ router.get("/pessoa/item", async (req, res) => {
 
 router.post("/pessoa/item",upload.fields([{name: "imagem"}, {name: "audio"}, {name:"video"}]), async (req, res) => {
     const nome = req.body.nome
+    const angulo = req.body.rotacao
     const classificacao = req.body.classificacao
     const divisaoSilabica = req.body.divisaoSilabica
     const imagens = req.files.imagem ? path.join('imagens', req.files.imagem[0].originalname).replace(/\\/g, '/') : null
     const audio = req.files.audio ? path.join('audios', req.files.audio[0].originalname).replace(/\\/g, '/') : null
     const video = req.files.video ? path.join('videos', req.files.video[0].originalname).replace(/\\/g, '/') : null
-    console.log(imagens)
-    console.log(video)
-    console.log(audio)
+
+    // const url = 'http://localhost:5000/rotate'
+    // const formData = new FormData();
+    // console.log("AQUI")
+
+    // formData.append('image', req.files.imagem[0])
+    // formData.append("angulo", angulo)
+
+    // const axiosConfig = {
+    //   headers: {
+    //     ...formData.getHeaders()
+    //   }
+    // }
+    // const imagemRotacionada = await axios.post(url, formData, axiosConfig)
+    // console.log(imagemRotacionada)
+
+    
     const item = new Item({
         nome,
         classificacao,
         divisaoSilabica,
         audio,
-        video
+        video,
       })
       const response = await ItemService.createItemToPessoa(req.session.userId, item, imagens)
       res.status(200).send(response)
---
+++ src/public/js/rotateImage.js
@@ -0,0 +1,5 @@
+    function rotateImage() {
+        const img = document.getElementById('imagemRotacao'); 
+        img.style.transform = 'rotate(60deg)';
+        console.log("AQUI")
+    }
