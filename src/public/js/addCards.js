5fbed0ce9e9b960af99be4402cac7ebcff7fdf42
Murilo <127051508+murilo587@users.noreply.github.com>
2024-06-16 16:18:26 -0300

    trabalhando na função de microfone e formulário

--- src/public/js/addCards.js
+++ src/public/js/addCards.js
@@ -1,1 +1,14 @@
 const divClickable = document.querySelector("div[id='video']")
+let div_imagem = document.querySelector('#imagem')
+let input_imagem = document.querySelector('#input-img')
+let div_video = document.querySelector('#video')
+let input_video = document.querySelector('#input-video')
+
+
+div_imagem.addEventListener('click', () => {
+    input_imagem.click()
+})
+
+div_video.addEventListener('click', () => {
+    input_video.click()
+})
