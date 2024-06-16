51ebc38d92869ab552d2e74df812989a100c2c02
Murilo <127051508+murilo587@users.noreply.github.com>
2024-06-16 18:19:59 -0300

    funcao de aparecer a imagem selecionada

--- src/public/js/addCards.js
+++ src/public/js/addCards.js
@@ -5,6 +5,8 @@ let div_video = document.querySelector('#video')
 let input_video = document.querySelector('#input-video')
 let submit = document.querySelector("#submit_a")
 let input_submit = document.querySelector("#enviar")
+let imagem_upload = document.querySelector('#img-upload')
+
 div_imagem.addEventListener('click', () => {
     input_imagem.click()
 })
@@ -15,4 +17,37 @@ div_video.addEventListener('click', () => {
 
 submit.addEventListener("click", () =>{
     input_submit.click()
+})
+
+
+//imagem no input
+
+input_imagem.addEventListener('change', function(event) {
+    const file = this.files[0]
+
+    if (file) {
+        const reader = new FileReader()
+
+        reader.onload = function () {
+            div_imagem.style.backgroundImage = `url('${reader.result}')`
+        }
+
+        reader.readAsDataURL(file)
+    }
+})
+
+input_video.addEventListener('change', function(event) {
+    const file = this.files[0]
+
+    if (file) {
+        const reader = new FileReader()
+
+        reader.onload = function () {
+            div_video.style.backgroundImage = `url('${reader.result}')`
+            console.log(imagem_upload)
+            imagem_upload.style.visibility = 'hidden'
+        }
+
+        reader.readAsDataURL(file)
+    }
 })
