bd84e73ef8088ab1c3920d2c0c9281aa9819b0ed
Murilo <127051508+murilo587@users.noreply.github.com>
2024-06-16 20:53:48 -0300

    funcao de caixa alta em letras

--- src/public/js/listas.js
+++ src/public/js/listas.js
@@ -1,13 +1,19 @@
 //modal listas
 
-modal = document.querySelector('#modal-listas')
-btnModal = document.querySelector('.addList')
-btnEnviar = document.querySelector('#enviar')
-btnFechar = document.querySelector('#fechar')
+let modal = document.querySelector('#modal-listas')
+let btnModal = document.querySelector('.addList')
+let btnEnviar = document.querySelector('#enviar')
+let btnFechar = document.querySelector('#fechar')
+let form = document.querySelector('#form-listas')
+let input_lista = document.querySelector('#nome_lista')
 
 btnModal.addEventListener('click', () => {
     modal.showModal()
     modal.classList.add('translate')
+})
+
+form.addEventListener('submit', function() {
+    input_lista.value = input_lista.value.toUpperCase()
 })
 
 btnFechar.addEventListener('click', () => {
